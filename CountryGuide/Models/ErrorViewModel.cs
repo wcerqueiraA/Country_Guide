6225f3c2aff5676534852a65d8dbf3809804cde0
Wellington <wellington.cerqueira32@gmail.com>
2026-03-12 12:58:02 -0300

    First Commit

---
+++ CountryGuide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CountryGuide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
