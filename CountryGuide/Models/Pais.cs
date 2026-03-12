13c62cf0cb6dbcc69da1e87bd13eda276cbd99b8
Wellington <wellington.cerqueira32@gmail.com>
2026-03-12 13:01:49 -0300

    feat: add Pais model

---
+++ CountryGuide/Models/Pais.cs
@@ -0,0 +1,11 @@
+﻿namespace CountryGuide.Models
+{
+    public class Pais
+    {
+        public string Nome { get; set; }
+        public string Capital { get; set; }
+        public List<string> Idioma { get; set; }
+        public Double Area { get; set;}
+        public Double Populacao { get; set;}
+    }
+}
