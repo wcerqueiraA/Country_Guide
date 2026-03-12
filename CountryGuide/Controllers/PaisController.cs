398ad3603bb92db523213151b72bbd556cc0c334
Wellington <wellington.cerqueira32@gmail.com>
2026-03-12 13:48:42 -0300

    feat: add PaisController

---
+++ CountryGuide/Controllers/PaisController.cs
@@ -0,0 +1,28 @@
+﻿using CountryGuide.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountryGuide.Controllers
+{
+    public class PaisController : Controller
+    {
+        private readonly PaisService _paisService;
+        public PaisController(PaisService paisService)
+        {
+            _paisService = paisService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string nome)
+        {
+            var pais = await _paisService.ObterPaisAsync(nome);
+            return View(pais);
+        }
+
+    }
+}
--- CountryGuide/Models/Pais.cs
+++ CountryGuide/Models/Pais.cs
@@ -1,11 +1,18 @@
-﻿namespace CountryGuide.Models
+﻿using System.Text.Json.Serialization;
+
+namespace CountryGuide.Models
 {
     public class Pais
     {
+        [JsonPropertyName("name")]
         public string Nome { get; set; }
+        [JsonPropertyName("capital")]
         public string Capital { get; set; }
+        [JsonPropertyName("languages")]
         public List<string> Idioma { get; set; }
+        [JsonPropertyName("area")]
         public Double Area { get; set;}
+        [JsonPropertyName("population")]
         public int Populacao { get; set;}
     }
 }
